f443fdcd9596c9192da93a8503c5d39ac1ee28f1
valgardg <valgardo20@ru.is>
2024-07-02 16:59:00 -0400

    updated: vite.config.ts according to tutorial, but broken due to resolve import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+      // src/index.ts is where we have exported the component
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "EeastereggComponent",
+      // the name of the output files when the build is run
+      fileName: "eeasteregg-component",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shoulnd't be bundled
+      // into your library
+      external: ["vue"],
+      output: {
+        // provide global variables to use in the UMD build
+        globals: {
+          vue: "Vue",
+        }
+      }
+    }
+  }
 })
